9a56fef88ad074a573545f43ec031d28881810bd
zodal <zodal@outlook.com>
2021-01-09 20:51:38 +0800

    (api) 添加注释

--- api/src/main/kotlin/github/afezeria/hymn/api/conf/WebConfig.kt
+++ api/src/main/kotlin/github/afezeria/hymn/api/conf/WebConfig.kt
@@ -13,6 +13,9 @@ import java.nio.file.Paths
 class WebConfig : WebMvcConfigurer {
     /**
      * 自定义页面代码存放在应用工作目录下的的static-resource目录下
+     * 压缩文档存放在工作目录下的archive目录中
+     * 启动时检查数据库core_custom_page表中的项和md5值是否和archive目录中的压缩文件一致
+     * 不一致时从文件服务器下载压缩文件
      */
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         val path = Paths.get("${System.getProperty("user.dir")}/static-resource")
