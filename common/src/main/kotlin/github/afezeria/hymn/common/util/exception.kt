c95940a75532b58acd0b29448d45b9bd612da8d9
zodal <zodal@outlook.com>
2021-01-10 19:42:46 +0800

    (common) 修改异常构建参数

--- common/src/main/kotlin/github/afezeria/hymn/common/util/exception.kt
+++ common/src/main/kotlin/github/afezeria/hymn/common/util/exception.kt
@@ -6,20 +6,27 @@ import github.afezeria.hymn.common.constant.ResultCode
 /**
  * @author afezeria
  */
-sealed class PlatformException(val code: ResultCode, override val message: String = code.msg) :
-    RuntimeException(message)
+sealed class PlatformException(
+    val code: ResultCode,
+    override val message: String = code.msg,
+    cause: Throwable? = null
+) :
+    RuntimeException(message, cause)
 
-class PermissionDeniedException(msg: String = ResultCode.PERMISSION_DENIED.msg) :
-    PlatformException(ResultCode.PERMISSION_DENIED, msg)
+class PermissionDeniedException(
+    msg: String = ResultCode.PERMISSION_DENIED.msg,
+    cause: Throwable? = null
+) :
+    PlatformException(ResultCode.PERMISSION_DENIED, msg, cause)
 
-class UnauthorizedException(msg: String = ResultCode.UNAUTHORIZED.msg) :
-    PlatformException(ResultCode.UNAUTHORIZED, msg)
+class UnauthorizedException(msg: String = ResultCode.UNAUTHORIZED.msg, cause: Throwable? = null) :
+    PlatformException(ResultCode.UNAUTHORIZED, msg, cause)
 
-class DataNotFoundException(msg: String = ResultCode.DATA_NOT_FOUND.msg) :
-    PlatformException(ResultCode.DATA_NOT_FOUND, "$msg not found")
+class DataNotFoundException(msg: String = ResultCode.DATA_NOT_FOUND.msg, cause: Throwable? = null) :
+    PlatformException(ResultCode.DATA_NOT_FOUND, "$msg not found", cause)
 
-class InnerException(msg: String = ResultCode.INNER_ERROR.msg) :
-    PlatformException(ResultCode.INNER_ERROR, msg)
+class InnerException(msg: String = ResultCode.INNER_ERROR.msg, cause: Throwable? = null) :
+    PlatformException(ResultCode.INNER_ERROR, msg, cause)
 
-class BusinessException(msg: String = ResultCode.BUSINESS_FAILED.msg) :
-    PlatformException(ResultCode.BUSINESS_FAILED, msg)
+class BusinessException(msg: String = ResultCode.BUSINESS_FAILED.msg, cause: Throwable? = null) :
+    PlatformException(ResultCode.BUSINESS_FAILED, msg, cause)
