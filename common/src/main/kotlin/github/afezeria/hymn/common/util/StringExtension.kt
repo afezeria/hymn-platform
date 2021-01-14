893acab65c12b53c69e30c7f2974835552c0f7cb
zodal <zodal@outlook.com>
2021-01-14 11:10:18 +0800

    (common) 修改扩展函数参数

--- common/src/main/kotlin/github/afezeria/hymn/common/util/StringExtension.kt
+++ common/src/main/kotlin/github/afezeria/hymn/common/util/StringExtension.kt
@@ -4,8 +4,8 @@ package github.afezeria.hymn.common.util
  * @author afezeria
  */
 private val UNAVAILABLE_CHARACTER_SET = setOf('\\', '?', '*', '<', '"', ':', '>', '/')
-fun String.isValidFileName(str: String): Boolean {
-    if (str.length > 189) return false
-    str.forEach { if (UNAVAILABLE_CHARACTER_SET.contains(it)) return false }
+fun String.isValidFileName(): Boolean {
+    if (length > 189) return false
+    forEach { if (UNAVAILABLE_CHARACTER_SET.contains(it)) return false }
     return true
 }
