11e4efe238eeb0f684e89bf9b770f7a97df435ed
zodal <zodal@outlook.com>
2021-01-14 22:40:15 +0800

    (common) 添加扩展函数，添加测试

--- common/src/main/kotlin/github/afezeria/hymn/common/util/StringExtension.kt
+++ common/src/main/kotlin/github/afezeria/hymn/common/util/StringExtension.kt
@@ -1,11 +1,18 @@
 package github.afezeria.hymn.common.util
 
+import java.util.*
+
 /**
  * @author afezeria
  */
-private val UNAVAILABLE_CHARACTER_SET = setOf('\\', '?', '*', '<', '"', ':', '>', '/')
-fun String.isValidFileName(): Boolean {
-    if (length > 189) return false
-    forEach { if (UNAVAILABLE_CHARACTER_SET.contains(it)) return false }
-    return true
-}
+private val FILE_NAME_REGEX = "[^\\x00-\\x1f\\x7f\\\\?*<\":>/]{1,255}".toRegex()
+fun String.isValidFileName(): Boolean = matches(FILE_NAME_REGEX)
+fun String.throwIfFileNameInvalid() =
+    takeIf { matches(FILE_NAME_REGEX) } ?: throw BusinessException("$this 不是有效的文件名")
+
+
+private val BUCKET_REGEX = "[-a-z0-9.]{3,40}".toRegex()
+fun String.isValidBucketName(): Boolean = matches(BUCKET_REGEX)
+fun String.throwIfBucketNameInvalid() =
+    takeIf { matches(BUCKET_REGEX) } ?: throw InnerException("$this 不是有效的bucket名称")
+
---
+++ common/src/test/kotlin/github/afezeria/hymn/common/KGenericContainer.kt
@@ -0,0 +1,8 @@
+package github.afezeria.hymn.common
+
+import org.testcontainers.containers.GenericContainer
+
+/**
+ * @author afezeria
+ */
+class KGenericContainer(imageName: String) : GenericContainer<KGenericContainer>(imageName)
