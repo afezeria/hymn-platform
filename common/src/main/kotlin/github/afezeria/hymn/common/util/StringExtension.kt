dec77a5d13edd429340172f0859066ab202fdadb
zodal <zodal@outlook.com>
2021-01-23 22:23:17 +0800

    (common) 修改文件名长度限制

--- common/src/main/kotlin/github/afezeria/hymn/common/util/StringExtension.kt
+++ common/src/main/kotlin/github/afezeria/hymn/common/util/StringExtension.kt
@@ -5,7 +5,7 @@ import java.util.*
 /**
  * @author afezeria
  */
-private val FILE_NAME_REGEX = "[^\\x00-\\x1f\\x7f\\\\?*<\":>/]{1,255}".toRegex()
+private val FILE_NAME_REGEX = "[^\\x00-\\x1f\\x7f\\\\?*<\":>/]{1,222}".toRegex()
 fun String.isValidFileName(): Boolean = matches(FILE_NAME_REGEX)
 fun String.throwIfFileNameInvalid() =
     takeIf { matches(FILE_NAME_REGEX) } ?: throw BusinessException("$this 不是有效的文件名")
