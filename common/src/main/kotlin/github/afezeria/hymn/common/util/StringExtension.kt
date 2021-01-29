527f0d31f479e92da4f2b9c59e39e4d6baaaaef8
zodal <zodal@outlook.com>
2021-01-29 17:17:09 +0800

    (common) 添加string扩展函数

--- common/src/main/kotlin/github/afezeria/hymn/common/util/StringExtension.kt
+++ common/src/main/kotlin/github/afezeria/hymn/common/util/StringExtension.kt
@@ -28,3 +28,26 @@ fun String.msgByPairs(vararg pairs: Pair<String, Any?>): String {
 fun String.msgByPair(field: String, value: Any?): String {
     return "$this [$field:$value]"
 }
+
+private val camelizeRegex = Regex("(.?(?<![a-zA-Z0-9])\\w)")
+
+fun String.camelize(): String {
+    return this.replace(camelizeRegex) {
+        it.value.last().toUpperCase().toString()
+    }
+}
+
+fun String.lCamelize(): String {
+    return this.replace(camelizeRegex) {
+        it.value.last().toUpperCase().toString()
+    }
+}
+
+private val underscoreRegex = Regex("([a-z]+|[A-Z][a-z]+|[A-Z]+|[0-9]+)")
+fun String.underscore(): String {
+    return underscoreRegex.findAll(this).map { it.value.toLowerCase() }.joinToString("_")
+}
+
+fun String.dasherize(): String {
+    return this.underscore().replace("_", "-")
+}
