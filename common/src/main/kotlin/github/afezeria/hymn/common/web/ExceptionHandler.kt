6f308412ce3697600c1947f263497d204b7f341a
zodal <zodal@outlook.com>
2021-01-31 18:31:16 +0800

    (common) 更新异常处理逻辑

--- common/src/main/kotlin/github/afezeria/hymn/common/web/ExceptionHandler.kt
+++ common/src/main/kotlin/github/afezeria/hymn/common/web/ExceptionHandler.kt
@@ -1,49 +1,46 @@
 package github.afezeria.hymn.common.web
 
 import github.afezeria.hymn.common.constant.AccountType
-import github.afezeria.hymn.common.exception.*
-import github.afezeria.hymn.common.platform.SessionService
+import github.afezeria.hymn.common.exception.BusinessException
+import github.afezeria.hymn.common.exception.PermissionDeniedException
+import github.afezeria.hymn.common.exception.PlatformException
+import github.afezeria.hymn.common.exception.UnauthorizedException
+import github.afezeria.hymn.common.platform.Session
 import mu.KLogging
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import java.io.PrintWriter
 import java.io.StringWriter
+import javax.servlet.ServletException
 
 /**
  * @author afezeria
  */
 @ControllerAdvice
-class ExceptionHandler : ResponseEntityExceptionHandler() {
+class ExceptionHandler {
     companion object : KLogging()
 
-    @Autowired
-    lateinit var sessionService: SessionService
-
     @ExceptionHandler(PlatformException::class)
-    @ResponseBody
     fun platformExceptionHandler(
         ex: PlatformException,
     ): ResponseEntity<ErrorResponse> {
         logger.warn(ex.message, ex)
-        val session = sessionService.getSession()
+        val session = Session.getInstance()
         val resp = if (session.accountType == AccountType.ADMIN) {
             val stringWriter = StringWriter()
             ex.printStackTrace(PrintWriter(stringWriter))
             ErrorResponse(ex.code, ex.message, stringWriter.toString())
         } else {
             if (ex is BusinessException ||
-                ex is DataNotFoundException ||
                 ex is PermissionDeniedException ||
                 ex is UnauthorizedException
             ) {
                 ErrorResponse(ex.code, ex.message)
             } else {
-                ErrorResponse(message = "服务器内部错误")
+                ErrorResponse(message = "内部错误")
             }
         }
         return ResponseEntity(resp, HttpStatus.valueOf(ex.httpCode))
@@ -52,9 +49,10 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(Throwable::class)
     fun handler(ex: Throwable): ResponseEntity<ErrorResponse> {
+        if (ex is ServletException) throw ex
         logger.warn(ex.message, ex)
         return ResponseEntity(
-            ErrorResponse(message = "服务器内部错误"),
+            ErrorResponse(message = "内部错误"),
             HttpStatus.INTERNAL_SERVER_ERROR
         )
     }
