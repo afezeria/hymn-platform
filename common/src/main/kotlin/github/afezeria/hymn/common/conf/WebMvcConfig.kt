2efbae341b44f1fe7bda9884d9717c9abfea0620
zodal <zodal@outlook.com>
2021-02-04 18:13:49 +0800

    (common) 添加接口版本号配置

---
+++ common/src/main/kotlin/github/afezeria/hymn/common/conf/WebMvcConfig.kt
@@ -0,0 +1,16 @@
+package github.afezeria.hymn.common.conf
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
+
+
+/**
+ * @author afezeria
+ */
+@Configuration
+class WebMvcConfig : WebMvcRegistrations {
+    override fun getRequestMappingHandlerMapping(): RequestMappingHandlerMapping {
+        return ApiRequestMappingHandlerMapping()
+    }
+}
