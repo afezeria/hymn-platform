d28187547572d146ed340ebbebd9f61a9de480c5
zodal <zodal@outlook.com>
2021-04-05 21:28:37 +0800

    (script) 修改锁定逻辑，添加清空所有缓存的方法

--- script/src/main/kotlin/com/github/afezeria/hymn/script/platform/Cache.kt
+++ script/src/main/kotlin/com/github/afezeria/hymn/script/platform/Cache.kt
@@ -10,6 +10,10 @@ import java.time.Duration
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReentrantLock
+import java.util.concurrent.locks.ReentrantReadWriteLock
+import kotlin.concurrent.read
+import kotlin.concurrent.withLock
+import kotlin.concurrent.write
 
 /**
  * @author afezeria
@@ -30,55 +34,59 @@ abstract class SourceCache<V> {
             ExpiryPolicyBuilder.timeToIdleExpiration(Duration.ofSeconds(30))
         )
     )
-    private val sourceCache: MutableMap<String, V> = ConcurrentHashMap()
+    private var sourceCache: MutableMap<String, V> = ConcurrentHashMap()
+    private val globalCacheLock = ReentrantReadWriteLock(true)
+
+    fun cleanAllCache() {
+        globalCacheLock.write {
+            sourceCache = ConcurrentHashMap()
+        }
+    }
 
     fun clean(key: String) {
-        val lock = ReentrantLock(true).let {
-            lockCache.putIfAbsent(key, it) ?: it
-        }
-        if (lock.tryLock()) {
-            try {
+        globalCacheLock.read {
+            ReentrantLock(true).let {
+                lockCache.putIfAbsent(key, it) ?: it
+            }.withLock {
                 sourceCache.remove(key)
-            } finally {
-                lock.unlock()
             }
         }
     }
 
     fun getOrPut(key: String, dataProvider: () -> V): V {
-        var value = sourceCache[key]
-        if (value == null) {
-            val lock = ReentrantLock(true).let {
-                lockCache.putIfAbsent(key, it) ?: it
-            }
-            if (lock.tryLock()) {
-                try {
+        globalCacheLock.read {
+            var value: V? = sourceCache[key]
+            if (value == null) {
+                ReentrantLock(true).let {
+                    lockCache.putIfAbsent(key, it) ?: it
+                }.withLock {
                     value = sourceCache[key]
                     if (value == null) {
                         value = dataProvider()
-                        sourceCache[key] = value
+                        sourceCache[key] = requireNotNull(value)
                     }
-                } finally {
-                    lock.unlock()
                 }
             }
+            return requireNotNull(value)
         }
-        return requireNotNull(value)
     }
 
 }
 
 /**
+ * 触发器代码缓存
  * key为对象id
  */
 object TriggerCache : SourceCache<List<TriggerSourceWithTime>>()
 
 /**
+ * 自定义api代码缓存
  * key为接口api
  */
 object ApiCache : SourceCache<SourceWithTime>()
 
 /**
+ * 自定义函数代码缓存
  * key为函数id
  */
 object FunctionCache : SourceCache<SourceWithTime>()
